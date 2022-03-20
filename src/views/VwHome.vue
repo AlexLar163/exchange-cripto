b14384aabe312b9b2cf2bec282ca604de3f37e37
Alexlar163 <alexlar163@gmail.com>
2022-03-20 01:30:56 -0500

    Add initial views

---
+++ src/views/VwHome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <px-assets-table />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable.vue";
+
+export default {
+  name: "CmHome",
+  components: { PxAssetsTable },
+};
+</script>
